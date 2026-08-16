c59727dee67514e47c9e5a5310d815c71f6cc592
Murat Altınışık <112017427+murataltinisik@users.noreply.github.com>
2026-08-16 15:50:38 +0300

    Add UsePotentiometer sketch for light control based on potentiometer input

---
+++ UsePotentiometer/UsePotentiometer.ino
@@ -0,0 +1,44 @@
+// Variables
+int a0DigitalValue = 0;
+int yellowPin = 3, bluePin = 4, greenPin = 5;
+
+void setup() {
+
+  // Set Pin Modes
+  pinMode(A0, INPUT);
+  pinMode(bluePin, OUTPUT);
+  pinMode(greenPin, OUTPUT);
+  pinMode(yellowPin, OUTPUT);
+
+  // Serial Begin
+  Serial.begin(9600);
+}
+
+void loop() {
+  // Read Digital Value
+  a0DigitalValue = analogRead(A0);
+
+  if(a0DigitalValue > 0 && a0DigitalValue <= 240){
+    // Close Light
+    digitalWrite(bluePin, LOW);
+    digitalWrite(greenPin, LOW);
+
+    // Open Light
+    digitalWrite(yellowPin, HIGH);
+  }
+  else if (a0DigitalValue > 240 && a0DigitalValue <= 330){
+    // Close Light
+    digitalWrite(greenPin, LOW);
+    digitalWrite(yellowPin, LOW);
+
+    // Open Light
+    digitalWrite(bluePin, HIGH);
+  }else{
+    // Close Light
+    digitalWrite(bluePin, LOW);
+    digitalWrite(yellowPin, LOW);
+
+    // Open Light
+    digitalWrite(greenPin, HIGH);
+  }
+}
